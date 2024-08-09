b8db15cd5ac5041129b7bacedec5feb496d5d78e
manuel <manuelpcrdz@gmail.com>
2024-08-08 23:24:15 -0600

    Contact us fix

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -43,7 +43,6 @@ const Layout = ({ isDrawerOpen, setIsDrawerOpen }) => {
   };
   const openDrawer = () => {
     setIsDrawerOpen(true);
-    document.body.style.overflowY = "hidden";
   };
   const closeDrawer = () => {
     setIsDrawerOpen(false);
@@ -118,7 +117,6 @@ const Layout = ({ isDrawerOpen, setIsDrawerOpen }) => {
           </li>
         </ul>
       </div>
-
       <div
         className={
           isDrawerOpen
@@ -129,68 +127,121 @@ const Layout = ({ isDrawerOpen, setIsDrawerOpen }) => {
         <span className="close-btn" onClick={closeDrawer}>
           <IoMdClose />
         </span>
-        <h3 className="contact-title">¡Contáctanos por tu medio preferido!</h3>
-        <div className="social-media-container">
-          <p className="social-media-title">Por nuestras redes sociales</p>
-          <div className="social-media">
+        <div className="contact-info-box">
+          <h3 className="contact-title">
+            ¡Contáctanos por tu medio preferido!
+          </h3>
+          <div className="social-media-container">
+            <p className="social-media-title">Por nuestras redes sociales</p>
+            <div className="social-media">
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                className="facebook"
+              >
+                <FaFacebook className="s-icon f" />
+                <p className="media-text">Facebook</p>
+              </a>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                className="instagram"
+              >
+                <FaInstagram className="s-icon i" />
+                <p className="media-text">Instagram</p>
+              </a>
+              <a
+                href="https://web.whatsapp.com"
+                target="_blank"
+                className="whatsapp"
+              >
+                <FaWhatsapp className="s-icon w" />
+                <p className="media-text">WhatsApp</p>
+              </a>
+            </div>
+          </div>
+          <div className="common-media-container">
+            <p className="common-title">Por correo o telefono</p>
+            <div className="common-container">
+              <a
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=contacto@cfi.com.mx&su=¡Quiero trabajar con CFI!"
+                target="_blank"
+                className="mail"
+              >
+                <SiGmail className="s-icon m" />
+                <p className="mail-address">contacto@cfi.com.mx</p>
+              </a>
+              <a href="tel:+1234567890" target="_blank" className="phone">
+                <FaPhone className="s-icon p" />
+                <p className="phone-number">+52 472 100 0801</p>
+              </a>
+            </div>
+          </div>
+          <div className="address-container">
+            <p className="address-title">También puedes visitarnos en:</p>
             <a
-              href="https://www.facebook.com"
-              target="_blank"
-              className="facebook"
+              href="https://www.google.com.mx/maps/place/Miguel+Hidalgo+42,+Centro,+36100+Silao+de+la+Victoria,+Gto./@20.9481327,-101.430521,17z/data=!3m1!4b1!4m6!3m5!1s0x842b9c7dbd5cf005:0xb311410c7be43ad3!8m2!3d20.9481328!4d-101.4256501!16s%2Fg%2F11c1kff3wl?entry=ttu"
+              target="_noblank"
+              className="address"
             >
-              <FaFacebook className="s-icon f" />
-              <p className="media-text">Facebook</p>
-            </a>
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              className="instagram"
-            >
-              <FaInstagram className="s-icon i" />
-              <p className="media-text">Instagram</p>
-            </a>
-            <a
-              href="https://web.whatsapp.com"
-              target="_blank"
-              className="whatsapp"
-            >
-              <FaWhatsapp className="s-icon w" />
-              <p className="media-text">WhatsApp</p>
+              <SiGooglemaps className="s-icon a" />
+              <p className="addres-text">
+                Calle Hidalgo #42, Silao, Guanajuato, México, C.P. 36100
+              </p>
             </a>
           </div>
         </div>
-        <div className="common-media-container">
-          <p className="common-title">Por correo o telefono</p>
-          <div className="common-container">
-            <a
-              href="https://mail.google.com/mail/?view=cm&fs=1&to=contacto@cfi.com.mx&su=¡Quiero trabajar con CFI!"
-              target="_blank"
-              className="mail"
-            >
-              <SiGmail className="s-icon m" />
-              <p className="mail-address">contacto@cfi.com.mx</p>
-            </a>
-            <a href="tel:+1234567890" target="_blank" className="phone">
-              <FaPhone className="s-icon p" />
-              <p className="phone-number">+52 472 100 0801</p>
-            </a>
+        <form onSubmit={(e) => sendMessage(e)} className="contact-form">
+          <p className="contact-form-title">O puedes enviarnos un mensaje:</p>
+          <div className="form-container">
+            <div className="input-label">
+              <input
+                type="text"
+                className="name inputfield"
+                id="name"
+                autoComplete="off"
+                required
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+              <label htmlFor="name" className="label-text">
+                Nombre
+              </label>
+            </div>
+            <div className="input-label">
+              <input
+                type="text"
+                className="email inputfield"
+                id="email"
+                autoComplete="off"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+              <label htmlFor="email" className="label-text">
+                Correo electrónico
+              </label>
+            </div>
+            <div className="input-label">
+              <input
+                type="text"
+                className="text inputfield"
+                id="text"
+                autoComplete="off"
+                required
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+              />
+              <label htmlFor="text" className="label-text">
+                Escribe tu mensaje
+              </label>
+            </div>
+            <div className="container-btn">
+              <input type="submit" value="Enviar" className="send-button" />
+            </div>
           </div>
-        </div>
-        <div className="address-container">
-          <p className="address-title">También puedes visitarnos en:</p>
-          <a
-            href="https://www.google.com.mx/maps/place/Miguel+Hidalgo+42,+Centro,+36100+Silao+de+la+Victoria,+Gto./@20.9481327,-101.430521,17z/data=!3m1!4b1!4m6!3m5!1s0x842b9c7dbd5cf005:0xb311410c7be43ad3!8m2!3d20.9481328!4d-101.4256501!16s%2Fg%2F11c1kff3wl?entry=ttu"
-            target="_noblank"
-            className="address"
-          >
-            <SiGooglemaps className="s-icon a" />
-            <p className="addres-text">
-              Calle Hidalgo #42, Silao, Guanajuato, México, C.P. 36100
-            </p>
-          </a>
-        </div>
+        </form>
       </div>
-
       <header className="header">
         <NavLink to="/" className="logo-container home-link">
           <img className="logo-img" src={logo} alt="company-logo" />
